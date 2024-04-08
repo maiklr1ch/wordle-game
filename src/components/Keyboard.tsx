6f679756727e1a55d7c91f77bd406552a79f9964
maiklr1ch <tmchenkodan@gmail.com>
2024-04-08 07:12:31 +0300

    beta 0.1

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,30 +1,46 @@
 import {FaDeleteLeft} from "react-icons/fa6";
 import React from "react";
+import {LetterState, PointerState} from "./Table";
 
 const rows = ["qwertyuiop", " asdfghjkl", "  zxcvbnm-"];
 const buttons = rows.map(row => row.split(""));
 
 type Props = {
-    onBackspace: () => void,
-    onPressed: (button: string) => void
+    onBackspace: (pointer: PointerState) => void,
+    onPressed: (button: string, pointer: PointerState) => void,
+    onEnter: (pointer: PointerState) => void,
+    onNewGame: () => void,
+    pointer: PointerState,
+    letterStates: { [letter: string]: LetterState },
 };
 
-export const Keyboard = ({ onBackspace, onPressed }: Props) => {
+export const Keyboard = ({ onBackspace, onPressed, onEnter, onNewGame, pointer, letterStates }: Props) => {
     return (
         <div className="keyboard">
             <table>
                 <tbody>
                 {buttons.map((buttonRow, buttonRowIndex) =>
                     <tr>
-                    {buttonRow.map((button, buttonIndex) => {
-                        if (buttonIndex + 1 === buttonRow.length && buttonRowIndex + 1 === buttons.length)
-                            return <td onClick={() => onBackspace()} key="backspace"><FaDeleteLeft/></td>
-                        else
-                            return <td onClick={() => onPressed(button)} key={button}>{button}</td>
+                        {buttonRow.map((button, buttonIndex) => {
+                            if (buttonIndex + 1 === buttonRow.length && buttonRowIndex + 1 === buttons.length)
+                                return <td onClick={() => onBackspace(pointer)} key="backspace"><FaDeleteLeft/></td>
+                            else
+                                return <td className={letterStates[button]} onClick={() => onPressed(button, pointer)}
+                                           key={button}>{button}</td>
                         })
-                    }
+                        }
                     </tr>
                 )}
+                <tr>
+                    <td colSpan={10} onClick={() => onEnter(pointer)}>
+                        enter
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={10} onClick={() => onNewGame()}>
+                        new word
+                    </td>
+                </tr>
                 </tbody>
             </table>
         </div>
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {Keyboard} from "./Keyboard";
 import {range} from "../utils/array";
+import {getRandomWord, isProper} from "../utils/dictionary";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const WORD_LENGTH = 5;
 const ROW_COUNT = 6;
 
+export type LetterState = 'correct' | 'semi-correct' | 'incorrect';
+
 type CellState = {
     letter: string,
-    state?: 'correct' | 'semi-correct' | 'incorrect',
+    state?: LetterState
 }
 
 type Board = CellState[][];
+
+export type PointerState = {
+    row: number,
+    col: number
+};
 
 const deepCopyBoard = (board: Board): Board => JSON.parse(JSON.stringify(board));
 
@@ -23,53 +33,156 @@ const getEmptyBoard = (): Board =>
         range(WORD_LENGTH).map(() => getEmptyCell())
     );
 
+const getStartPointer = (): PointerState => ({
+    row: 0,
+    col: 0
+});
+
 export const Table = () => {
     const [board, setBoard] = useState<Board>(getEmptyBoard());
-    const [currentRow, setCurrentRow] = useState<number>(0);
-    const handleBackspace = () => {
+    const [pointer, setPointer] = useState<PointerState>(getStartPointer());
+    const [correctWord, setCorrectWord] = useState<string>(getRandomWord());
+    const [isWin, setIsWin] = useState<boolean>(false);
+
+    useEffect(() => {
+        if(isWin)
+            return;
+        const handleKeydown = (ev: KeyboardEvent) => {
+            const key = ev.key.toLowerCase();
+            if(key === "backspace")
+                handleBackspace(pointer);
+            if(key === "enter")
+                handleEnter(pointer);
+            if('a' <= key && 'z' >= key && key.length === 1)
+                handlePressed(key, pointer);
+            if(key === "arrowleft")
+                setPointer((prevState: PointerState) => {
+                    return { row: prevState.row, col: prevState.col ? prevState.col - 1 : 0 };
+                });
+            if(key === "arrowright")
+                setPointer((prevState: PointerState) => {
+                    return { row: prevState.row, col: prevState.col + 1 === WORD_LENGTH ? prevState.col : prevState.col + 1 };
+                });
+        };
+
+        document.addEventListener('keydown', handleKeydown);
+        return () => document.removeEventListener('keydown', handleKeydown);
+    }, [pointer, isWin]);
+
+    useEffect(() => {
+        if(isWin)
+        {
+            const startNewGame = () => {
+                clearTimeout(timer);
+                setBoard(getEmptyBoard());
+                setPointer(getStartPointer());
+                setCorrectWord(getRandomWord());
+                setIsWin(false);
+            };
+            toast.success(`You win! The correct word was: ${correctWord.toUpperCase()}\nNew game starts in 10s`, { theme: "dark", autoClose: 10000, onClose: startNewGame, onClick: startNewGame });
+            const timer = setTimeout(startNewGame, 10000);
+        }
+    }, [isWin]);
+
+    const currentWord = useMemo(() => {
+        return board[pointer.row]
+            .map((cell) => cell.letter)
+            .join('')
+    }, [board, pointer]);
+
+    const handleBackspace = (pointer: PointerState) => {
         setBoard((prevState) => {
            const nextState = deepCopyBoard(prevState);
-           const row = nextState[currentRow];
-           for(let i = 0; i < WORD_LENGTH; i++)
-           {
-               if(row[i].letter === '' && i > 0) {
-                   row[i-1].letter = '';
-                   break;
-               }
-
-               if(i + 1 === WORD_LENGTH) {
-                   row[i].letter = '';
-                   break;
-               }
-           }
+           nextState[pointer.row][pointer.col].letter = "";
            return nextState;
+        });
+        setPointer((prevState: PointerState) => {
+            return { row: prevState.row, col: prevState.col ? prevState.col - 1 : 0 };
         });
     }
 
-    const handlePressed = (button: string) => {
+    const handlePressed = (button: string, pointer: PointerState) => {
         setBoard((prevState) => {
             const nextState: Board = deepCopyBoard(prevState);
-            for (const cell of nextState[currentRow]) {
-                if(!cell.letter)
-                {
-                    cell.letter = button;
-                    break;
-                }
-            }
+            nextState[pointer.row][pointer.col].letter = button;
             return nextState;
+        });
+        setPointer((prevState: PointerState) => {
+            return { row: prevState.row, col: prevState.col + 1 === WORD_LENGTH ? prevState.col : prevState.col + 1 };
         });
     };
 
+    const handleEnter = (pointer: PointerState) => {
+        if (currentWord.length === WORD_LENGTH)
+        {
+            if (currentWord === correctWord)
+            {
+                setBoard((prevState: Board) => {
+                    const nextState = deepCopyBoard(prevState);
+                    nextState[pointer.row].forEach((cell) => {
+                        cell.state = 'correct';
+                    });
+                    return nextState;
+                });
+                setIsWin(true);
+            }
+            else if(isProper(currentWord))
+            {
+                setBoard((prevState: Board) => {
+                    const nextState = deepCopyBoard(prevState);
+                    nextState[pointer.row].forEach((cell, index) => {
+                        if (cell.letter === correctWord[index])
+                            cell.state = 'correct';
+                        else if (correctWord.includes(cell.letter))
+                            cell.state = 'semi-correct';
+                        else
+                            cell.state = 'incorrect';
+                    });
+                    return nextState;
+                });
+                setPointer((prevState: PointerState) => {
+                    return { row: prevState.row + 1, col: 0 };
+                });
+            }
+            else
+                toast.error(`The word is not proper! Try: ${['PEACE', 'OCEAN'][Math.floor(Math.random() * 2)]}`, { theme: "dark" });
+        }
+        else
+            toast.error("Not enough letters!", { theme: "dark" });
+    }
+
+    const handleNewGame = () => {
+        toast.info('New game started', { theme: 'dark' });
+        setBoard(getEmptyBoard());
+        setPointer(getStartPointer());
+        setCorrectWord(getRandomWord());
+        setIsWin(false);
+    };
+
+    const letterStates = useMemo<{ [letter: string]: LetterState }>(() => {
+        return board.flat().reduce<{ [key: string]: LetterState }>((prev, cur) => {
+            const newLetterState: LetterState | undefined = prev[cur.letter] === 'correct' ? 'correct' : cur.state;
+            if(newLetterState)
+                return {
+                    ...prev,
+                    [cur.letter]: newLetterState
+                }
+            else
+                return prev;
+        }, {});
+    }, [board]);
+
     return (
         <div>
+            Correct word is {correctWord}
             <div className="wrapper">
                 <table>
                     <tbody>
-                    {board.map(row =>
+                    {board.map((row, rowIndex) =>
                         <tr>
-                            {row.map(value =>
-                                <td className={value.state}>
-                                    {value.letter}
+                            {row.map((cell, cellIndex) =>
+                                <td className={`${cell.state} ${rowIndex === pointer.row && cellIndex === pointer.col && 'pointer'}`}>
+                                    {cell.letter}
                                 </td>
                             )}
                         </tr>
@@ -77,7 +190,19 @@ export const Table = () => {
                     </tbody>
                 </table>
             </div>
-            <Keyboard onBackspace={handleBackspace} onPressed={handlePressed} />
+            <Keyboard onBackspace={handleBackspace} onPressed={handlePressed} onEnter={handleEnter} onNewGame={handleNewGame} pointer={pointer} letterStates={letterStates} />
+            <ToastContainer
+                position="top-center"
+                autoClose={1000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover={false}
+                theme="dark"
+            />
         </div>
     );
 };
